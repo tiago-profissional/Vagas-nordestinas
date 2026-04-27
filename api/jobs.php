cfb405e8ddda785d6ae3cf2305f06eadbf4c9df9
Tiago Ramon <tiago.profissional.trabalho@gmail.com>
2026-04-27 15:45:22 -0300

    Deploy para Vercel

--- api/jobs.php
+++ api/jobs.php
@@ -1,4 +1,5 @@
 <?php
+// CORS HEADERS - OBRIGATÓRIO
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
